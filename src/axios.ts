bd93369df65e7ebe4fb60de462b98c971a3ebd3b
nyarko-dennis <ndenniskofi@gmail.com>
2022-09-23 20:47:45 +0000

    chore(environment vars): Added production url for production build in a .env variable

--- src/axios.ts
+++ src/axios.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://fp-partner-api.devsnode.xyz/api/v0/dashboard";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
